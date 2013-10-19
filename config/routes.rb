56d04aad93c47da4dceefe22a015e869fd678a85
FearlessLeader <medsoft_objects@yahoo.com>
2013-10-19 12:49:57 -0700

    Added custom routes for auth and feed.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Treebook::Application.routes.draw do
   devise_for :users
+  
+  devise_scope :user do
+    get 'register', to: 'devise/registrations#new', as: :register
+    get 'login',    to: 'devise/sessions#new',      as: :login
+    get 'logout',   to: 'devise/sessions#destroy', as: :logout
+  end
 
   resources :statuses
+  get 'feed', to: 'statuses#index', as: :feed
 
   # Devise install instruction (#2):
   # 'Ensure you have defined root_url to *something* in your config/routes.rb.'
