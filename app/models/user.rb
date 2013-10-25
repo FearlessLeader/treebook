d39b6e19dc28be018d5fd4d6fb6ee215ead9d79a
FearlessLeader <medsoft_objects@yahoo.com>
2013-10-25 05:18:07 -0700

    Added Gravatar, added styles to statuses.css.scss, fixed show.html.erb 'time_ago' link.

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,10 @@ class User < ActiveRecord::Base
   def full_name
   	first_name + " " + last_name
   end
+  
+  def gravatar_url
+    strip_down_email = email.strip.downcase
+    hash = Digest::MD5.hexdigest(strip_down_email)
+    "http://gravatar.com/avatar/#{hash}"
+  end
 end
