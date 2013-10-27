eb710add394f5f308b90e1dba63067eb7de99239
FearlessLeader <medsoft_objects@yahoo.com>
2013-10-27 11:13:17 -0700

    Scoped status updates to the current user on create and update and updated tests in statuses_controller_test.rb to match treebook project files.

--- test/functional/statuses_controller_test.rb
+++ test/functional/statuses_controller_test.rb
@@ -29,41 +29,68 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to new_user_session_path
   end
 
-  test "should create status when user logged in" do
+  test "should create status when logged in" do
     sign_in users(:jason)
     
     assert_difference('Status.count') do
       post :create, status: { content: @status.content }
     end
-
+    
     assert_redirected_to status_path(assigns(:status))
   end
   
-  test "should get edit status when user logged in" do
+  test "should create status for the current user when logged in" do
     sign_in users(:jason)
-    get :edit, id: @status
-    assert_response :success
+    
+    assert_difference('Status.count') do
+      post :create, status: { content: @status.content, user_id: users(:jim).id }
+    end
+
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
   end
   
-  test "should redirect update status when user not logged in" do
-    sign_in users(:jason)
-    put :update, id: @status, status: { content: @status.content }
-    assert_redirected_to new_user_session_path
-  end  
-
   test "should show status" do
     get :show, id: @status
     assert_response :success
   end
+  
+  test "should redirect edit when not logged in"  do
+    get :edit, id: @status
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
 
-  test "should get edit" do
+  test "should get edit when logged in" do
+    sign_in users(:jason)
     get :edit, id: @status
     assert_response :success
   end
+  
+  test "should redirect status update when not logged in" do
+    put :update, id: @status, status: { content: @status.content }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end  
 
-  test "should update status" do
+  test "should update status when logged in" do
+    sign_in users(:jason)
     put :update, id: @status, status: { content: @status.content }
     assert_redirected_to status_path(assigns(:status))
+  end
+  
+  test "should update status for the current user when logged in" do
+    sign_in users(:jason)
+    put :update, id: @status, status: { content: @status.content, user_id: users(:jim).id }
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
+  end
+  
+  test "should not update the status if nothing has changed" do
+    sign_in users(:jason)
+    put :update, id: @status
+    assert_redirected_to status_path(assigns(:status))
+    assert_equal assigns(:status).user_id, users(:jason).id
   end
 
   test "should destroy status" do
